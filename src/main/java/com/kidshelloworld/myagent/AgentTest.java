a546666c74f52066dd40a96626c06b7e598ea084
huangtiande <huangtiande@rfchina.com>
2019-07-02 17:22:22 +0800

    add hello method to Person

--- src/main/java/com/kidshelloworld/myagent/AgentTest.java
+++ src/main/java/com/kidshelloworld/myagent/AgentTest.java
@@ -5,9 +5,10 @@ package com.kidshelloworld.myagent;
  * create_date: 2019-7-2
  */
 public class AgentTest {
-	public static void main(String[] args) {
-		String abc = "abc";
-		System.out.println(abc);
-		System.out.println(new Person());
+	public static void main(String[] args) throws Exception {
+		String name = "agent";
+		Person person = new Person();
+		System.out.print("invoke hello: ");
+		Person.class.getMethod("hello", String.class).invoke(person, name);
 	}
 }
--- src/main/java/com/kidshelloworld/myagent/MyTransformer.java
+++ src/main/java/com/kidshelloworld/myagent/MyTransformer.java
@@ -1,5 +1,10 @@
 package com.kidshelloworld.myagent;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -13,6 +18,19 @@ public class MyTransformer implements ClassFileTransformer {
 	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
 			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 		System.out.println("transform class name: " + className + ", classloader: " + loader.toString());
+		className = className.replace("/", ".");
+		if (className.equals("com.kidshelloworld.myagent.Person")) {
+			try {
+				CtClass ctclass = ClassPool.getDefault().get(className);
+				CtMethod helloM = CtNewMethod.make(
+						"public void hello(String name){ System.out.println(\"hello \"+name);}", ctclass);
+				ctclass.addMethod(helloM);
+				System.out.println("add method hello to class: " + className);
+				return ctclass.toBytecode();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 }
